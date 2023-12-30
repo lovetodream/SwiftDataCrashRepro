49b06a40ba4c1bb7c35668b939b761fa2d00e898
Timo <38291523+lovetodream@users.noreply.github.com>
2023-12-30 10:37:29 +0100

    feat: add crash repro

--- SwiftDataCrashRepro/ContentView.swift
+++ SwiftDataCrashRepro/ContentView.swift
@@ -6,19 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
+
+@Model
+final class MyModel {
+    var other: MyOtherModel?
+
+    init() {
+        self.other = other
+    }
+}
+
+@Model
+final class MyOtherModel {
+    var name: String = ""
+    @Relationship
+    var models: [MyModel]?
+
+    init(name: String) {
+        self.name = name
+    }
+}
 
 struct ContentView: View {
+    @Query(FetchDescriptor<MyModel>(sortBy: [SortDescriptor(\MyModel.other?.name)])) // crashes
+    // @Query // doesn't crash
+    private var models: [MyModel]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(models) { model in
+            Text(model.other?.name ?? "None")
         }
-        .padding()
+        .overlay {
+            if models.isEmpty {
+                ContentUnavailableView("No entries in list", systemImage: "magnifyingglass")
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: [MyModel.self, MyOtherModel.self], inMemory: true)
 }
