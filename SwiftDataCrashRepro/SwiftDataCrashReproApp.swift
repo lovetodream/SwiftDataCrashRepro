5bfebf203193f75e30b511937820fa314b93ca94
Timo <38291523+lovetodream@users.noreply.github.com>
2023-12-30 10:25:08 +0100

    Initial Commit

---
+++ SwiftDataCrashRepro/SwiftDataCrashReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCrashReproApp.swift
+//  SwiftDataCrashRepro
+//
+//  Created by Timo Zacherl on 30.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCrashReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
